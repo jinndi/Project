c8e8eb66690f07cee3b330818f17921115ae96e4
Alexander <mihaylov36rus@gmail.com>
2021-06-14 16:06:13 +0300

    Прошел 1-е задание

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert(2);
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?","");
+
+const personalMovieDB ={
+  count: numberOfFilms,
+  movies: {},
+  actors: {}, 
+  genres: [],
+  privat: false
+};
+
+const lastFilms = prompt("Один из последних просмотренных фильмов?",""),
+  ocenka = prompt("На сколько оцените его?",""),
+  lastFilms1 = prompt("Один из последних просмотренных фильмов?",""),
+  ocenka1 = prompt("На сколько оцените его?","");
+
+personalMovieDB.movies[lastFilms]  = ocenka;
+personalMovieDB.movies[lastFilms1]  = ocenka1;
+
+console.log(personalMovieDB);
